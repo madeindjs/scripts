3510db0405148363fe912902bde5b2d24145186d
madeindjs <madeindjs@gmail.com>
2016-08-24 09:22:42 +0200

    add argparse

--- print_doe.py
+++ print_doe.py
@@ -12,9 +12,9 @@
 	updated: 2016/08/24
 
 """
+import argparse
 # require pywin32 (download at https://sourceforge.net/projects/pywin32/ )
-import win32api 
-import win32print
+import win32api, win32print
 
 
 
@@ -28,4 +28,25 @@ def print_file(filename):
 
 
 
-print_file("C:/Users/rousseaua/Desktop/DOE Metro Clermont/DESP/BOUTEILLES/Plan_2312066_230715.pdf")
+
+def main():
+	# parse arg to find file(s)
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-f", "--file", help="print a given file")
+	parser.add_argument("-d", "--directory", help="print a given directory")
+	args = parser.parse_args()
+
+	if args.directory:# print the given directory
+		pass
+
+	elif args.file:# print the given file
+		print_file(args.file)
+
+	else:# print a test file
+		print_file("C:/Users/rousseaua/Desktop/DOE Metro Clermont/DESP/BOUTEILLES/Plan_2312066_230715.pdf")
+
+
+
+
+if __name__ == '__main__':
+	main()
