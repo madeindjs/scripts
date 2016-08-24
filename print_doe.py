ceed773caddbfb57bbd8134cffcf59347a366397
madeindjs <madeindjs@gmail.com>
2016-08-24 10:15:00 +0200

    add copy arg

--- print_doe.py
+++ print_doe.py
@@ -18,13 +18,14 @@ import win32api, win32print
 
 
 
-def print_file(filename):
+def print_file(filename, copy=1):
 	"""print a file on the default printer"""
 	try:
-		win32api.ShellExecute ( 0, "print", filename,
-			# If this is None, the default printer will  be used anyway.
-			'/d:"%s"' % win32print.GetDefaultPrinter (),
-			".", 0 )
+		for _ in range(copy):
+			win32api.ShellExecute ( 0, "print", filename,
+				# If this is None, the default printer will  be used anyway.
+				'/d:"%s"' % win32print.GetDefaultPrinter (),
+				".", 0 )
 		print("[*] request to print %s" % filename)
 	except pywintypes.error:
 		print("[ ] failed to print %s (maybe a wrong url?)" % filename)
@@ -32,7 +33,7 @@ def print_file(filename):
 
 
 
-def print_dir(directory):
+def print_dir(directory, copy=1):
 	"""print all files on the default printer"""
 	directory = directory.replace('\\', '/')
 	# get all files in the folder
@@ -40,7 +41,7 @@ def print_dir(directory):
 		files=[directory+"/"+file for file in os.listdir(directory) if not os.path.isdir(file)]
 		print("[*] search file into %s" % directory)
 		for file in files:
-			print_file(file)
+			print_file(file, copy)
 	except FileNotFoundError:
 		print("[ ] failed to find files into %s (maybe a wrong url?)" % directory)
 	
@@ -50,18 +51,22 @@ def print_dir(directory):
 def main():
 	# parse arg to find file(s)
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-f", "--file", help="print a given file")
-	parser.add_argument("-d", "--directory", help="print a given directory")
+	parser.add_argument("-f", "--file", type=str, help="print a given file")
+	parser.add_argument("-d", "--directory",type=str, help="print a given directory")
+	parser.add_argument("-n", "--number",type=int, help="Number of copy to print")
 	args = parser.parse_args()
 
+	copy = args.number if args.number else 1
+
 	if args.directory:# print the given directory
-		print_dir(args.directory)
+		print_dir(args.directory, copy)
 
 	elif args.file:# print the given file
-		print_file(args.file)
+		print_file(args.file, copy)
 
 	else:# print a test file
-		print_file("C:/Users/rousseaua/Desktop/DOE Metro Clermont/DESP/BOUTEILLES/Plan_2312066_230715.pdf")
+		url = "C:/Users/rousseaua/Desktop/DOE Metro Clermont/DESP/BOUTEILLES/Plan_2312066_230715.pdf"
+		print_file(url, copy)
 
 
 
