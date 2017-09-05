8475fd8b3b9e07a294a835cff8ba655c0bd8cfd0
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2017-09-05 15:16:46 +0200

    Add TABLE_NAME parameter & usage message

--- mysql_search_column.sh
+++ mysql_search_column.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-#
+
 # Search given columns between all MySQL tables 
 
-mysql -e "SELECT DISTINCT TABLE_NAME, COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME LIKE '%${1}%'"
+display_usage() { 
+  echo "Search given columns between all MySQL tables." 
+  echo -e "\nUsage:" 
+  echo -e '\t $1 as COLUMN_NAME'
+  echo -e '\t $2 as TABLE_NAME'
+}
+
+if [ -z $1 ]; then
+  display_usage
+  exit 1
+fi
+
+
+QUERY="SELECT DISTINCT TABLE_NAME, COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME LIKE '%${1}%'"
+
+if [ -n $2 ]; then
+  QUERY="$QUERY AND TABLE_NAME LIKE '%$2%'"
+fi
+
+
+mysql -e "$QUERY"
