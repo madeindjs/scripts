dfbb7ad1f23512666ff5d95508f39cbeb657980c
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2017-09-19 17:07:00 +0200

    improve scripts

--- tomatoes_to_hours.rb
+++ tomatoes_to_hours.rb
@@ -5,4 +5,4 @@ raise ArgumentError.new('Please enter a number') if ARGV.count == 0
 
 qty  = ARGV[0].chomp.to_f
 time = (qty * (25.0/60.0)).round(1)
-puts "#{qty} tomatoes is equivalent to #{time} hours"
+puts "#{qty.to_i} tomatoes is equivalent to #{time} hours"
