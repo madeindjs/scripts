a6cb5b001f88e738cd3deb7e881df1ab6ea38ad8
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-01-02 14:53:15 +0100

    add option to change database from command line

--- gac_current_database.sh
+++ gac_current_database.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-# Display the current branch connected to Car-fleet
+# Display the current database connected to Car-fleet
 function gac_get_current_database()
 {
     grep -e '^database.params.dbname' application/default/configs/application.ini 2> /dev/null | tail -1 | sed -e 's/database.params.dbname *= *//g'
 }
 
-echo "$(gac_get_current_database)"
+
+if [ -z $1 ]; then
+  # if no argument provided, just show current database
+  echo "$(gac_get_current_database)"
+else
+  # if argument is provided, change the current database connected to argument passed
+  CURRENT="$(gac_get_current_database)"
+  FROM="database.params.dbname *= *${CURRENT}"
+  TO="database.params.dbname = ${1}"
+  sed -ie "s/${FROM}/${TO}/g" application/default/configs/application.ini
+  # set default password for the support account
+  echo "UPDATE user SET password = '02cd4e217924d29efdf84abb31ed211e' WHERE login = 'support@gac-technology.com'" | docker exec -i mysql5.6_1 mysql -uroot -psecret "$1"
+fi
